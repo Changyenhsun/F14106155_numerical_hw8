4966ac6896c7ef67a9979b1d20492d05a586ea3e
Iris <f14106155@gs.ncku.edu.tw>
2025-05-26 12:32:39 +0800

    修改8_1, 8_2

--- 8_2.py
+++ 8_2.py
@@ -8,11 +8,9 @@ phi_0 = lambda x: 1
 phi_1 = lambda x: x
 phi_2 = lambda x: x**2
 
-# 內積函數 <f, g> = ∫ f(x) * g(x) dx from -1 to 1
 def inner_product(f1, f2):
     return integrate.quad(lambda x: f1(x) * f2(x), -1, 1)[0]
 
-# 計算矩陣與右邊常數項
 A = np.array([
     [inner_product(phi_0, phi_0), inner_product(phi_0, phi_1), inner_product(phi_0, phi_2)],
     [inner_product(phi_1, phi_0), inner_product(phi_1, phi_1), inner_product(phi_1, phi_2)],
@@ -25,11 +23,18 @@ b = np.array([
     inner_product(f, phi_2)
 ])
 
-# 解聯立方程式得到 a0, a1, a2
 coeffs = np.linalg.solve(A, b)
 
-# 顯示結果
+def P2(x):
+    return coeffs[0] + coeffs[1] * x + coeffs[2] * x**2
+
+def error_function(x):
+    return (f(x) - P2(x))**2
+
+sse, _ = integrate.quad(error_function, -1, 1)
+
+
 print()
-print(f"a0 = {coeffs[0]:.3f}, a1 = {coeffs[1]:.3f}, a2 = {coeffs[2]:.3f}")
-print(f"P2(x) = {coeffs[0]:.3f} + {coeffs[1]:.3f}x + ({coeffs[2]:.3f})x^2")
+print(f"P2(x) = {coeffs[0]:.5f} + {coeffs[1]:.5f}x + ({coeffs[2]:.5f})x^2")
+print(f"error = {sse:.5f}")
 print()
